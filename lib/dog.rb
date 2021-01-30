e6ea905ddab28254ef50c0beaed76917d005db15
Michael Penn <mwpenn94@gmail.com>
2021-01-30 22:05:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
  
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
  
   def self.all
@@ -20,7 +20,7 @@ class Dog
 		end
 	end	
 
-	def self.save
+	def save
 		@@all << self		
 	end
 
